e9a0f414d8dac3007a73f833996dc9a62cd7af12
Felix Rieseberg <felix@felixrieseberg.com>
2015-05-27 14:26:07 -0700

    Install VCPython, Swig

--- bootstrap/windows.ps1
+++ bootstrap/windows.ps1
@@ -80,6 +80,14 @@ function InstallVirtualEnv
     {
         throw "$(Get-Date -format t): Could not run pip. Please install Python 2.7 manually by visiting www.python.org. Aborting."
     }
+}
+
+function InstallVCPython {
+    "$(Get-Date -format t): Installing VC Compiler for Python 2.7..."
+    "Insallation of the VC Compiler has to be manual. Please visit http://aka.ms/vcpython27"
+    "to download and install the compiler. Once you're done, please continue this bootstrapper"
+    "by pressing any key."
+    $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 } 
 
 function IsAdministrator
@@ -100,6 +108,29 @@ function SetupLetsEncryptVenv
     "$(Get-Date -format t): Creating virtualenv directory..."
     cd ..
     virtualenv -p C:\python27\python.exe venv
+}
+
+function InstallSwig
+{
+    "$(Get-Date -format t): Installing Swig..."
+    "Insallation of the Swig has to be manual. Please visit http://aka.ms/swig3"
+    "to download Swig. Unzip SWIG in a folder of your choice and enter it here:"
+    "ZIP."
+    $swigZiplocation = Read-Host "Please enter the path to the folder containing swig.exe:"
+
+    "$(Get-Date -format t): Adding Swig to path..."
+    [Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $swigZiplocation, [System.EnvironmentVariableTarget]::Machine)
+
+    "$(Get-Date -format t): Reloading path for current session"
+    # Reload the PATH environment variable for the current session
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+
+}
+
+function SetupSwigEnvVars
+{
+    $env:PYTHON_INCLUDE = "C:\Python27\include"
+    $env:PYTHON_LIB = "C:\Python27\libs\python27.lib"
 }
 
 # ----------------------------------------------------------------------------------------------------------
@@ -142,8 +173,6 @@ if (-Not (IsPythonInstalled))
         {
             "$(Get-Date -format t): Python successfully installed."
             AddPythonToPath
-            InstallVirtualEnv
-            SetupLetsEncryptVenv
         } else {
             throw "$(Get-Date -format t): Python 2.7 Installation failed. Please install it manually by visiting www.python.org. Aborting."
         }
@@ -154,3 +183,9 @@ if (-Not (IsPythonInstalled))
 {
     "$(Get-Date -format t): Python 2.7 is installed. We only took a look, please install it if you're surprised."
 }
+
+InstallVirtualEnv
+SetupLetsEncryptVenv
+InstallVCPython
+InstallSwig
+SetupSwigEnvVars
